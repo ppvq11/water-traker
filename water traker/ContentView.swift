fd0442907758154e587e99fc35b771aa237ea4de
ppvq11 <muai0466@gmail.com>
2023-10-05 12:08:37 +0300

    Initial Commit

---
+++ water traker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  water traker
+//
+//  Created by Muna Aiman Al-hajj on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ water traker/water_trakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_trakerApp.swift
+//  water traker
+//
+//  Created by Muna Aiman Al-hajj on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct water_trakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
